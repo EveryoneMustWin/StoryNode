216c76f5e9e251df87147a65c45898b25f100293
Oscar Gillespie <Oscar.E.Gillespie@gmail.com>
2012-03-12 14:16:34 +0000

    got the node lookup hash working

--- StoryNode.js
+++ StoryNode.js
@@ -1,15 +1,19 @@
 
-function StoryNode(t) {
+function StoryNode(t, i) {
   this.text = t;
+  this.uid = i;
 }
 
 function startStory() {
-  var rootNode = new StoryNode("Once upon a time");
+  var rootNode = new StoryNode("Once upon a time", 1);
 
-  rootNode[1] = new StoryNode("A beautiful princess");
-  rootNode[2] = new StoryNode("A brave prince");
-  rootNode[3] = new StoryNode("A timid squire");
-  rootNode[4] = new StoryNode("A grumpy bus driver");
+  rootNode[1] = new StoryNode("A beautiful princess", 2);
+  rootNode[2] = new StoryNode("A brave prince", 3);
+  rootNode[3] = new StoryNode("A timid squire", 4);
+  rootNode[4] = new StoryNode("A grumpy bus driver", 5);
+
+  rootNode[1][1] = new StoryNode("who lived in a lofty tower in a magnificent castle", 6);
+  rootNode[1][2] = new StoryNode("who lived in a yurt", 7);
 
   return rootNode;
 }
